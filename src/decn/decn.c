8af9701e156d0b255cc6b7b791285fe8949277e0
Jeff Wang <jeff.wang@utexas.edu>
2019-04-04 03:36:35 -0400

    bugfix for entering numbers such as 0.33
    this is a partial revert of commit 8550049335b8152993

--- src/decn/decn.c
+++ src/decn/decn.c
@@ -191,6 +191,13 @@ void build_dec80(dec80* dest, const char* signif_str, int16_t exponent){
 				}
 			}
 			nibble_i++;
+			//track number of digits R of decimal point
+			//must do this before changing is_zero
+			if (num_lr_points == 0 &&
+			   (curr_sign == SIGN_ZERO_SEEN_POINT || curr_sign == SIGN_NEG_ZERO_SEEN_POINT))
+			{
+				num_lr_points = -1;
+			}
 			//track sign
 			if (curr_sign == SIGN_ZERO){
 				curr_sign = SIGN_POS;
